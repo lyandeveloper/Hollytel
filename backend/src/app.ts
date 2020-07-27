0ddc59b5198ec1635852889eb4cd1d3311532fd6
lyandeveloper <tutors3d@gmail.com>
2020-07-27 16:08:40 -0300

    feat: Error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
-import express from 'express';
+import express, {
+  Request,
+  Response,
+  ErrorRequestHandler,
+  NextFunction,
+  request,
+  response,
+} from 'express';
 import routes from './routes';
 
 class App {
-  public server = express.application;
+  public server: express.Application;
 
   constructor() {
     this.server = express();
@@ -12,10 +19,28 @@ class App {
 
   private middlewares(): void {
     this.server.use(express.json());
+    this.server.use(
+      (
+        err: ErrorRequestHandler,
+        req: Request,
+        res: Response,
+        next: Function
+      ) => {
+        if (req.xhr) {
+          res.status(500).send({ error: 'Something went wrong...' });
+        } else {
+          next(err);
+        }
+      }
+    );
   }
 
   private routes(): void {
     this.server.use(routes);
+
+    this.server.use((req: Request, res: Response, next: NextFunction) => {
+      res.status(404).send({ error: 'Not found' });
+    });
   }
 }
 
