33ff43e257ea6a595335f54b193452f842cb6940
lyandeveloper <tutors3d@gmail.com>
2020-08-04 12:17:07 -0300

    feat: style bottom tab

--- mobile/src/Routes.tsx
+++ mobile/src/Routes.tsx
@@ -4,6 +4,8 @@ import {
   CardStyleInterpolators,
 } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import Login from './screens/Login';
 import SignUp from './screens/SignUp';
 import Home from './screens/Home';
@@ -14,11 +16,58 @@ import Bookings from './screens/Bookings';
 const Main: React.FC = () => {
   const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Favoritos" component={Favorites} />
-      <Tab.Screen name="Ofertas" component={Deals} />
-      <Tab.Screen name="Agendamentos" component={Bookings} />
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#f22e62ff',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="md-home" color={color} size={size} />
+            ) : (
+              <Ionicons name="md-home-outline" color={color} size={size} />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Favoritos"
+        component={Favorites}
+        options={{
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="heart" color={color} size={size} />
+            ) : (
+              <Ionicons name="heart-outline" color={color} size={size} />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Ofertas"
+        component={Deals}
+        options={{
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="cash" color={color} size={size} />
+            ) : (
+              <Ionicons name="cash-outline" color={color} size={size} />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Agendamentos"
+        component={Bookings}
+        options={{
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="calendar" color={color} size={size} />
+            ) : (
+              <Ionicons name="calendar-outline" color={color} size={size} />
+            ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
